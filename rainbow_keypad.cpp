740664260ec17b9e50dbb69ec7850ad2f31eb778
Josema <josemalive@gmail.com>
2021-03-19 22:52:24 +0100

    Organizado un poco el código. Divido en bloques

--- rainbow_keypad.cpp
+++ rainbow_keypad.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "pico/stdlib.h"
 
 #include "pico_rgb_keypad.hpp"
@@ -7,163 +9,196 @@ using namespace pimoroni;
 
 PicoRGBKeypad teclado;
 
-uint8_t rojo = 0, verde = 0, azul = 0;
+uint8_t rojo = 0;
+uint8_t verde = 0;
+uint8_t azul = 0;
+uint8_t boton = 0;
+
 uint8_t color_rojo[7] = {0, 0, 0, 0, 0, 0, 0};
 uint8_t color_verde[7] = {0, 0, 0, 0, 0, 0, 0};
 uint8_t color_azul[7] = {0, 0, 0, 0, 0, 0, 0};
-uint8_t boton = 6;
-const uint16_t RANGO = 8;
 uint16_t indice = 0;
+
+const uint8_t RANGO = 17;
+
 void rgb(double ratio);
 
 int main()
 {
-    stdio_init_all();
     teclado.init();
-    teclado.set_brightness(0.05f);
+    teclado.set_brightness(0.1f);
 
+    // ** Barrido diagonal V2 suave NO FUNCIONA
+    // while (true)
+    // {
+    //     rgb(indice / RANGO);
+    //     if (indice == 0) {
+    //         color_rojo[7] = color_rojo[0];
+    //         color_verde[7] = color_verde[0];
+    //         color_azul[7] = color_azul[0];
+    //     } else {
+    //         color_rojo[indice - 1] = color_rojo[indice];
+    //         color_verde[indice - 1] = color_verde[indice];
+    //         color_azul[indice - 1] = color_azul[indice];
+    //     }
+        
+    //     color_rojo[0] = rojo;
+    //     color_verde[0] = verde;
+    //     color_azul[0] = azul;
+
+    //     teclado.illuminate(0, color_rojo[0], color_verde[0], color_azul[0]);
+
+    //     teclado.illuminate(1, color_rojo[1], color_verde[1], color_azul[1]);
+    //     teclado.illuminate(4, color_rojo[1], color_verde[1], color_azul[1]);
+
+    //     teclado.illuminate(2, color_rojo[2], color_verde[2], color_azul[2]);
+    //     teclado.illuminate(5, color_rojo[2], color_verde[2], color_azul[2]);
+    //     teclado.illuminate(8, color_rojo[2], color_verde[2], color_azul[2]);
+
+    //     teclado.illuminate(3, color_rojo[3], color_verde[3], color_azul[3]);
+    //     teclado.illuminate(6, color_rojo[3], color_verde[3], color_azul[3]);
+    //     teclado.illuminate(9, color_rojo[3], color_verde[3], color_azul[3]);
+    //     teclado.illuminate(12, color_rojo[3], color_verde[3], color_azul[3]);
+
+    //     teclado.illuminate(7, color_rojo[4], color_verde[4], color_azul[4]);
+    //     teclado.illuminate(10, color_rojo[4], color_verde[4], color_azul[4]);
+    //     teclado.illuminate(13, color_rojo[4], color_verde[4], color_azul[4]);
+
+    //     teclado.illuminate(11, color_rojo[5], color_verde[5], color_azul[5]);
+    //     teclado.illuminate(14, color_rojo[5], color_verde[5], color_azul[5]);
+
+    //     teclado.illuminate(15, color_rojo[6], color_verde[6], color_azul[6]);
+
+    //     teclado.update();
+    //     sleep_ms(20);
+
+    //     indice++;
+    //     if (indice > 6) indice = 0;
+    // }
+    // ** Barrido diagonal V2 suave NO FUNCIONA
+
+    // ** Barrido diagonal suave NO FUNCIONA
+    // while (true)
+    // {
+    //     for (uint8_t i = 6; i > 0; i--)
+    //     {
+    //         rgb(indice / RANGO);
+    //         if (indice >= RANGO) {
+    //             indice = 0;
+    //         } else {
+    //             indice++;
+    //         }
+
+            
+                
+    //         color_rojo[0] = rojo;
+
+    //         color_verde[0] = verde;
+
+    //         color_azul[0] = azul;
+
+    //         teclado.illuminate(0, color_rojo[0], color_verde[0], color_azul[0]);
+
+    //         teclado.illuminate(1, color_rojo[1], color_verde[1], color_azul[1]);
+    //         teclado.illuminate(4, color_rojo[1], color_verde[1], color_azul[1]);
+
+    //         teclado.illuminate(2, color_rojo[2], color_verde[2], color_azul[2]);
+    //         teclado.illuminate(5, color_rojo[2], color_verde[2], color_azul[2]);
+    //         teclado.illuminate(8, color_rojo[2], color_verde[2], color_azul[2]);
+
+    //         teclado.illuminate(3, color_rojo[3], color_verde[3], color_azul[3]);
+    //         teclado.illuminate(6, color_rojo[3], color_verde[3], color_azul[3]);
+    //         teclado.illuminate(9, color_rojo[3], color_verde[3], color_azul[3]);
+    //         teclado.illuminate(12, color_rojo[3], color_verde[3], color_azul[3]);
+
+    //         teclado.illuminate(7, color_rojo[4], color_verde[4], color_azul[4]);
+    //         teclado.illuminate(10, color_rojo[4], color_verde[4], color_azul[4]);
+    //         teclado.illuminate(13, color_rojo[4], color_verde[4], color_azul[4]);
+
+    //         teclado.illuminate(11, color_rojo[5], color_verde[5], color_azul[5]);
+    //         teclado.illuminate(14, color_rojo[5], color_verde[5], color_azul[5]);
+
+    //         teclado.illuminate(15, color_rojo[6], color_verde[6], color_azul[6]);
+
+        
+    //         color_rojo[i + 1] = color_rojo[i];
+    //         color_verde[i + 1] = color_verde[i];
+    //         color_azul[i + 1] = color_azul[i];
+
+    //         teclado.update();
+    //         sleep_ms(20);
+    //     }
+
+        
+    // }
+    // ** Barrido diagonal suave NO FUNCIONA
+    
+
+    // *** Iluminación botón a botón
+    //   while (true){
+    //     for (double i = 0; i < RANGO; i++) {
+    //         rgb(i / RANGO);
+    //         if (boton >= 16) {
+    //             boton = 0;
+    //         }
+    //         teclado.illuminate(boton, rojo, verde, azul);
+    //         boton++;
+    //         teclado.update();
+    //         sleep_ms(6);
+    //     }
+    //   }
+    // *** Iluminación botón a botón
+
+    // *** Barrido en diagonal NO SUAVE
     while (true)
     {
-        rgb(indice / RANGO);
-        if (indice >= RANGO) {
-            indice = 0;
-        } else {
-            indice++;
-        }
-        // Barrido suave
-        // for (uint8_t i = 6; i < 0; i--)
-        // {
-        for (uint8_t i = 6; i > 0; i--)
+        for (double i = 0; i < RANGO; i++)
         {
-            /* code */color_rojo[i + 1] = color_rojo[i];
-            // printf(int()color_rojo[i]);
-        color_verde[i + 1] = color_verde[i];
-        color_azul[i + 1] = color_azul[i];
+            rgb(i / RANGO);
+            if (boton >= 7)
+            {
+                boton = 0;
+            }
+            switch (boton)
+            {
+            case 0:
+                teclado.illuminate(0, rojo, verde, azul);
+                break;
+            case 1:
+                teclado.illuminate(1, rojo, verde, azul);
+                teclado.illuminate(4, rojo, verde, azul);
+                break;
+            case 2:
+                teclado.illuminate(2, rojo, verde, azul);
+                teclado.illuminate(5, rojo, verde, azul);
+                teclado.illuminate(8, rojo, verde, azul);
+                break;
+            case 3:
+                teclado.illuminate(3, rojo, verde, azul);
+                teclado.illuminate(6, rojo, verde, azul);
+                teclado.illuminate(9, rojo, verde, azul);
+                teclado.illuminate(12, rojo, verde, azul);
+                break;
+            case 4:
+                teclado.illuminate(7, rojo, verde, azul);
+                teclado.illuminate(10, rojo, verde, azul);
+                teclado.illuminate(13, rojo, verde, azul);
+                break;
+            case 5:
+                teclado.illuminate(11, rojo, verde, azul);
+                teclado.illuminate(14, rojo, verde, azul);
+                break;
+            case 6:
+                teclado.illuminate(15, rojo, verde, azul);
+                break;
+            }
+            boton++;
+            teclado.update();
+            sleep_ms(50);
         }
-       
-        // }
-        color_rojo[0] = rojo;
-
-        color_verde[0] = verde;
-
-        color_azul[0] = azul;
-        // if (boton == 0)
-        // {
-        //     boton = 6;
-        // }
-        // else
-        // {
-        //     boton--;
-        // }
-
-        teclado.illuminate(0, color_rojo[0], color_verde[0], color_azul[0]);
-
-        teclado.illuminate(1, color_rojo[1], color_verde[1], color_azul[1]);
-        teclado.illuminate(4, color_rojo[1], color_verde[1], color_azul[1]);
-
-        teclado.illuminate(2, color_rojo[2], color_verde[2], color_azul[2]);
-        teclado.illuminate(5, color_rojo[2], color_verde[2], color_azul[2]);
-        teclado.illuminate(8, color_rojo[2], color_verde[2], color_azul[2]);
-
-        teclado.illuminate(3, color_rojo[3], color_verde[3], color_azul[3]);
-        teclado.illuminate(6, color_rojo[3], color_verde[3], color_azul[3]);
-        teclado.illuminate(9, color_rojo[3], color_verde[3], color_azul[3]);
-        teclado.illuminate(12, color_rojo[3], color_verde[3], color_azul[3]);
-
-        teclado.illuminate(7, color_rojo[4], color_verde[4], color_azul[4]);
-        teclado.illuminate(10, color_rojo[4], color_verde[4], color_azul[4]);
-        teclado.illuminate(13, color_rojo[4], color_verde[4], color_azul[4]);
-
-        teclado.illuminate(11, color_rojo[5], color_verde[5], color_azul[5]);
-        teclado.illuminate(14, color_rojo[5], color_verde[5], color_azul[5]);
-
-        teclado.illuminate(15, color_rojo[6], color_verde[6], color_azul[6]);
-
-        // Barrido botón a botón
-        // if (boton >= 16) {
-        //     boton = 0;
-        // }
-        // teclado.illuminate(boton, rojo, verde, azul);
-        // boton++;
-
-        // Barrido por lineas de arriba a abajo.
-        // if (boton >= 16) {
-        //     boton = 0;
-        // }
-        // teclado.illuminate(boton + 0, rojo, verde, azul);
-        // teclado.illuminate(boton + 1, rojo, verde, azul);
-        // teclado.illuminate(boton + 2, rojo, verde, azul);
-        // teclado.illuminate(boton + 3, rojo, verde, azul);
-        // boton += 4;
-        // teclado.update();
-        // sleep_ms(5);
-
-        // Barrido en diagonal
-        // if (boton >= 7) {
-        //     boton = 0;
-        // }
-        // switch (boton)
-        // {
-        // case 0:
-        //     teclado.illuminate(0, rojo, verde, azul);
-        //     break;
-        // case 1:
-        //     teclado.illuminate(1, rojo, verde, azul);
-        //     teclado.illuminate(4, rojo, verde, azul);
-        //     break;
-        // case 2:
-        //     teclado.illuminate(2, rojo, verde, azul);
-        //     teclado.illuminate(5, rojo, verde, azul);
-        //     teclado.illuminate(8, rojo, verde, azul);
-        //     break;
-        // case 3:
-        //     teclado.illuminate(3, rojo, verde, azul);
-        //     teclado.illuminate(6, rojo, verde, azul);
-        //     teclado.illuminate(9, rojo, verde, azul);
-        //     teclado.illuminate(12, rojo, verde, azul);
-        //     break;
-        // case 4:
-        //     teclado.illuminate(7, rojo, verde, azul);
-        //     teclado.illuminate(10, rojo, verde, azul);
-        //     teclado.illuminate(13, rojo, verde, azul);
-        //     break;
-        // case 5:
-        //     teclado.illuminate(11, rojo, verde, azul);
-        //     teclado.illuminate(14, rojo, verde, azul);
-        //     break;
-        // case 6:
-        //     teclado.illuminate(15, rojo, verde, azul);
-        //     break;
-
-        // }
-        // boton++;
-        // teclado.update();
-        // sleep_ms(35);
-
-        // Barrido botón a botón
-        // if (boton >= 16) {
-        //     boton = 0;
-        // }
-        // teclado.illuminate(boton, rojo, verde, azul);
-        // boton++;
-
-        // Barrido por lineas de arriba a abajo.
-        // if (boton >= 16) {
-        //     boton = 0;
-        // }
-        // teclado.illuminate(boton + 0, rojo, verde, azul);
-        // teclado.illuminate(boton + 1, rojo, verde, azul);
-        // teclado.illuminate(boton + 2, rojo, verde, azul);
-        // teclado.illuminate(boton + 3, rojo, verde, azul);
-        // boton += 4;
-        // teclado.update();
-        // sleep_ms(5);
-     teclado.update();
-    sleep_ms(200);
     }
-   
-
-return 0;
+    // *** Barrido en diagonal NO SUAVE
+    return 0;
 }
 
 // La siguiente función sale de aqui: https://stackoverflow.com/questions/40629345/fill-array-dynamicly-with-gradient-color-c
